15a2504083336a14c9efbc9ec51811965c8b018d
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-02-15 16:33:15 +0800

    update

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -39,9 +39,13 @@
     <!-- Static sidebar for desktop -->
     <div class="hidden lg:fixed lg:inset-y-0 lg:z-40 lg:flex lg:w-72 lg:flex-col">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+      <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200  px-6 pb-4">
         <div class="flex h-16 shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+            <img class="h-8 w-auto mr-4" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+            <p class="text-lg font-black text-gray-600">
+                Fund Tracer
+
+            </p>
         </div>
         <nav class="flex flex-1 flex-col">
             {{-- <ul class="list-none mb-2">
@@ -164,7 +168,7 @@
     </div>
 
     <div class="lg:pl-72">
-      <div class="sticky top-0 z-20 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+      <div class="sticky top-0 z-20 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200  px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
         <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
           <span class="sr-only">Open sidebar</span>
           <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -184,7 +188,7 @@
       </div>
 
       <main class="py-10">
-        <div class="px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl  m-auto px-4 sm:px-6 lg:px-8">
             {{$slot}}
           <!-- Your content -->
         </div>
