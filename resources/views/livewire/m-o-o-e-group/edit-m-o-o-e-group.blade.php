cf9ab2fc3056ffc12a0424dd52314d5ccb4be39b
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-03-03 14:17:52 +0800

    udpate button

--- resources/views/livewire/m-o-o-e-group/edit-m-o-o-e-group.blade.php
+++ resources/views/livewire/m-o-o-e-group/edit-m-o-o-e-group.blade.php
@@ -1,15 +1,8 @@
 <div>
     <x-management-layout>
         <div class="flex justify-end">
-            <a href="{{route('mooe.index')}}" class="rounded bg-gray-600 p-2 text-white flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
-                </svg>
+            <x-back-button :url="route('mooe.index')">BACK</x-back-button>
 
-                <span class="">
-                    Back
-                </span>
-            </a>
         </div>
         <p class="my-2 text-2xl italic text-primary-600 mb-4">
             Maintenance and Other Operating Expenses > {{$record->title}}
